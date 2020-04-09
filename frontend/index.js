6a60ee4965152d0500902360ff12378791410917
jona <jona@jona.com>
2020-04-09 19:51:01 +0300

    first commit

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+const menu_button=document.getElementById('menu-btn');
+const menu=document.querySelector('#navbar-list');
+
+menu_button.addEventListener('click',()=>{
+    menu.style.display="block";
+    menu.style.backgroundColor="";
+
+});
